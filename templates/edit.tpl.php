04ea73c8e136d32f285340f0afcb747cc7b6d317
BethsFaith <BethsFaith7@gmail.com>
2023-12-17 20:30:56 +0300

    A few refactoring moments

--- templates/edit.tpl.php
+++ templates/edit.tpl.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>AdminPanel</title>
-    <link rel="stylesheet" href="editstyle.css">
+    <link rel="stylesheet" href="styles/editstyle.css">
 </head>
 <body>
 <div id="wrapper">
